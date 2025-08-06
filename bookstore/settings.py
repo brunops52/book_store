87ee5931bb0449b5a4b27ca8796c869c140f0850
Bruno <bunros52@gmail.com>
2025-08-06 21:52:53 +0000

    commenting on the second DEBUG

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 
 #SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+# DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
